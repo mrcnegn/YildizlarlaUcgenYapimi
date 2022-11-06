a2e3e498894c1c71836d8514b2605d3c91a841e5
Mert sezgin <sezgincanmert@gmail.com>
2022-11-06 13:02:11 +0300

    Yıldızlarla Üçgen Yapımı
    Yıldızlarla Üçgen Yapımı

---
+++ YıldızlarİleUcgenYapimi/src/YıldızlarİleUcgenYapimi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class YýldýzlarÝleUcgenYapimi {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);	
+		
+		System.out.println("sayý giriniz: ");
+		int a=input.nextInt();
+		
+		for(int i=1;i<=a;i++) {
+			for(int k=1;k<=(a-i);k++) {
+			System.out.print(" ");
+		}
+			for(int j=1;j<=(2*i)-1;j++) {
+				System.out.print("*");
+			}
+			 System.out.println();
+	}
+	}
+}
